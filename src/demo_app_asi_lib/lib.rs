d15b1e587036d352ca0ecd64dc01e8c126033247
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-04-06 12:47:54 -0400

    feat: trigger rebuild

--- src/demo_app_asi_lib/lib.rs
+++ src/demo_app_asi_lib/lib.rs
@@ -5,3 +5,4 @@ pub fn lib_function() {
 // Adding an empty commit to trigger a build
 // Adding another empty commit to trigger a build
 //NOTE - This is a test commit to trigger a build
+//TODO - This is a test commit to trigger a build
